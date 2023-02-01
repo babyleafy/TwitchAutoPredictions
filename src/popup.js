cff596414af17713692caa7b8cf8df72cfea1eb0
Alex Lee <alfrost1010@gmail.com>
2023-02-01 02:02:10 -0500

    change

--- src/popup.js
+++ src/popup.js
@@ -2,10 +2,12 @@ function saveOptions() {
 	let bonus = document.getElementById('autoBonus').checked;
 	let bet = document.getElementById('autoBet').checked;
 	let betOptions = document.querySelector('input[name="betOptions"]:checked')?.value;
+	let pointSum = document.getElementById('points');
 	chrome.storage.sync.set({
 		bonus: bonus,
 		bet: bet,
 		betOptions: betOptions,
+		pointSum: pointSum,
 	}, function() {
 		// Update status to let user know options were saved.
 		let saveStatus = document.getElementById('status');
@@ -26,7 +28,7 @@ function saveOptions() {
 		tabs.forEach(function(tab) {
 			// Initializes handshake with potential twitch-clicker.js script inside the tab
 			chrome.tabs.sendMessage(tab.id,
-				{bonus: bonus, bet: bet, betOptions: betOptions}, function(msg) {
+				{bonus: bonus, bet: bet, betOptions: betOptions, pointSum: pointSum}, function(msg) {
 				if(chrome.runtime.lastError) { msg = {}; }
 				else { msg = msg || {}; }
 			});
@@ -34,7 +36,7 @@ function saveOptions() {
 	});
 
 	console.log("Options saved");
-	console.log("Bonus: " + bonus + " Bet: " + bet + " Bet Options: " + betOptions);
+	console.log("Bonus: " + bonus + " Bet: " + bet + " Bet Options: " + betOptions + "PointSum: " + pointSum);
 }
 
 // Restores Options state using the preferences saved in chrome.storage
@@ -42,10 +44,12 @@ function restoreOptions() {
 	chrome.storage.sync.get({
 		'bonus': false,
 		'bet': false,
-		'betOptions': null
+		'betOptions': null,
+		'pointSum': '0'
 	}, function(items) {
 		document.getElementById('autoBonus').checked = items.bonus;
 		document.getElementById('autoBet').checked = items.bet;
+		document.getElementById('points') = items.pointSum;
 		if (items.betOptions === "betPeople") {
 			document.getElementById('betPeople').checked = true;
 		} else if (items.betOptions === "betPoints") {
@@ -57,6 +61,18 @@ function restoreOptions() {
 	console.log("Options restored");
 
 }
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log(sender.tab);
+		if (request.increment == "addPoint")
+			currVal = parseInt(document.getElementById('points').innerHTML);
+			currVal += 1;
+			document.getElementById('points').innerHTML = currVal.toString();
+			sendResponse({confirm: "confirmed add"});
+	}
+  );
+  
 
 //On load
 document.addEventListener('DOMContentLoaded', restoreOptions); //Restores options from storage on loading
